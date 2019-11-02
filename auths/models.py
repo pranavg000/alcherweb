ee7f4bc52242a7ae3cf6d88905413da745eac0ab
Pranav <pranavgupta000@gmail.com>
2019-11-02 12:21:09 +0530

    initial commit

---
+++ auths/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
+
+
+class Interest(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+	
+		
+
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+	full_name = models.CharField(max_length=200)
+	phone = models.CharField(max_length=11)
+	college = models.CharField(max_length=100)
+	GENDER_CHOICES = [
+	('M', 'Male'),
+	('F', 'Female'),
+	]
+	gender = models.CharField(
+		choices = GENDER_CHOICES,
+		max_length=1,
+		default='M'
+		)
+	state = models.CharField(max_length=50)
+	city = models.CharField(max_length=100)
+	interests = models.ManyToManyField(Interest)
+
+	def __str__(self):
+		return self.user.username
+
+
---
+++ auths/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def register(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		email = request.POST['email']
+		User.objects.create_user(username, email, password)
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+		else:
+			return render(request, 'auths/signup.html')
+		
+		messages.success(request,f'Account created for {username}!')
+		return redirect('core:home')
+	else:
+		return render(request, 'auths/signup.html')
+
+
+
+	
