3fe15b03e88faca00a61647afd0ef2fd6a7fab20
Pranav <pranavgupta000@gmail.com>
2019-12-23 11:31:36 +0530

    started questionnare

--- auths/views.py
+++ auths/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
 
 def register(request):
+
 	if request.method == 'POST':
 		
 		username = request.POST['username']
@@ -17,7 +17,32 @@ def register(request):
 		else:
 			return render(request, 'auths/signup.html')
 		
-		messages.success(request,f'Account created for {username}!')
 		return redirect('auths:login')
 	else:
+		venues = Venue.objects.filter(user=request.user).order_by('-pk')
+		name_pattern = "A-Za-z ";
+		team_name_pattern = "A-Za-z0-9, ";
+		phone_pattern = "0-9";
 		return render(request, 'auths/signup.html')
+
+
+
+# def login(request):
+# 	username = password = ''
+# 	if request.POST:
+# 		username = request.POST['username']
+# 		password = request.POST['password']
+
+# 		user = authenticate(username=username, password=password)
+# 		if user is not None:
+# 			if user.is_active:
+# 				login(request, user)
+# 				ca_detail = CA_Detail.objects.get(user = request.user);
+# 				if ca_detail.ca_approval :
+# 					return redirect('ca:home')
+# 				elif ca_detail.ca_profile_complete :
+# 					return redirect('ca:pending')
+# 				else :
+# 					return redirect('ca:questionnare')
+
+# 	return render(request, 'auths/login.html')
