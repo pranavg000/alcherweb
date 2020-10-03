463c9eb99cab54e2d1f103cd3937784800fb4862
Pranav <pranavgupta000@gmail.com>
2020-10-03 21:38:40 +0530

    fixed warnings

--- auths/views.py
+++ auths/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .models import *
+from .models import Interest, Profile, CA_Detail
 from .tokens import account_activation_token
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -241,7 +241,7 @@ def account_activation_sent(request):
 
 
 def login(request):
-	username = password = ''
+	password = ''
 	if request.POST:
 		email = request.POST['ca_email']
 		password = request.POST['ca_password']
@@ -282,10 +282,10 @@ def login(request):
 			auth_login(request, user)
 			print(user.username)
 
-			ca_detail = CA_Detail.objects.filter(user = request.user);
+			ca_detail = CA_Detail.objects.filter(user = request.user)
 			if len(ca_detail) == 0:
 				CA_Detail.objects.create(user=request.user)
-				ca_detail = CA_Detail.objects.filter(user = request.user);
+				ca_detail = CA_Detail.objects.filter(user = request.user)
 			if ca_detail[0].ca_approval :
 				return redirect('ca:home')
 			elif ca_detail[0].ca_profile_complete :
