652c2dc7f7e58df569eb9f2ca6776c72c558af75
Ujjwal Rustagi <ujjwal24111999@gmail.com>
2021-02-09 18:21:47 +0530

    Edited staticfiles

--- alcher/settings.py
+++ alcher/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
 
 # Add these new lines
 STATICFILES_DIRS = (
@@ -146,6 +145,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = 'ca:home'
 LOGIN_URL = '/auths/facebook/callback/ṭ'
 
