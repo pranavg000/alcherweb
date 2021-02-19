3e35a6b0a22c73ecf263e9d624a6c4d0137044c5
Curious-Ujjwal <ujjwal24111999@gmail.com>
2021-02-19 10:47:23 +0530

    Added options for States

---
+++ auths/choices.py
@@ -0,0 +1,65 @@
+GENRE_CHOICES = {
+('Dance','Dance'),
+('Music','Music'),
+('Drama','Drama'),
+('Arts','Arts'),
+('Fashion','Fashion'),
+('Literary and Debate','Literary and Debate'),
+('Sports','Sports'),	
+}
+
+IDEA_CATEGORY_CHOICES = {
+('Competition', 'Competition'),
+('Workshop', 'Workshop'),
+('Hospitality', 'Hospitality'),
+('Artist', 'Artist'),
+('Crowd Experience', 'Crowd Experience'),
+('Other', 'Other'),
+}
+
+FAQ_CATEGORY_CHOICES = {
+('general', 'GENERAL'),
+('portal', 'PORTAL'),
+('competition', 'COMPETITIONS'),
+('accommodation', 'ACCOMMODATION'),
+('audition', 'CITY AUDITION'),
+}
+
+COMPLAINT_CATEGORY_CHOICES = {
+    ('General', 'General'),
+    ('Technical', 'Technical'),
+    ('Competition', 'Competition'),
+    ('Festival', 'Festival'),
+    ('Payment', 'Payment'),
+    }
+
+STATE_CHOICES = [
+    ('Andhra Pradesh', 'Andhra Pradesh'),
+    ('Arunachal Pradesh', 'Arunachal Pradesh'),
+    ('Assam', 'Assam'),
+    ('Bihar', 'Bihar'),
+    ('Chhattisgarh', 'Chhattisgarh'),
+    ('Goa', 'Goa'),
+    ('Gujarat', 'Gujarat'),
+    ('Haryana', 'Haryana'),
+    ('Himachal Pradesh', 'Himachal Pradesh'),
+    ('Jharkhand', 'Jharkhand'),
+    ('Karnataka', 'Karnataka'),
+    ('Kerala', 'Kerala'),
+    ('Madhya Pradesh', 'Madhya Pradesh'),
+    ('Maharashtra', 'Maharashtra'),
+    ('Manipur', 'Manipur'),
+    ('Meghalaya', 'Meghalaya'),
+    ('Mizoram', 'Mizoram'),
+    ('Nagaland', 'Nagaland'),
+    ('Odisha', 'Odisha'),
+    ('Punjab', 'Punjab'),
+    ('Rajasthan', 'Rajasthan'),
+    ('Sikkim', 'Sikkim'),
+    ('Tamil Nadu', 'Tamil Nadu'),
+    ('Telangana', 'Telangana'),
+    ('Tripura', 'Tripura'),
+    ('Uttar Pradesh', 'Uttar Pradesh'),
+    ('Uttarakhand', 'Uttarakhand'),
+    ('West Bengal', 'West Bengal'),
+]
